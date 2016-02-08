9fcdff5261076090b9ae6779f1fae9c99514ce4c
Stefan Stadlberger <github@email.stadlberger.com>
2016-02-08 22:26:27 +0100

    new: default config file

---
+++ config.default.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.bosh = {
+	url: 'http://URL_TO_SYSAP:5280/http-bind',
+	jid: 'JID@busch-jaeger.de',
+	resource: 'nodeapi',
+	password: 'PASSWORD'
+};
+
+module.exports = config;
