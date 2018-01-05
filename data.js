sstadlberger/home

commit 3488c1324ad74b1e42e2635d5dea14eb7674d344

change: unify all data in a single object change: weather data is no longer a separate data set but included with all other data in the actuators object note: maybe rename the actuators object to something more general

data.js
@@ -141,7 +141,7 @@ daynight:
  * @param {string} channel - channel id (e.g. ch0000)
  * @param {string} datapoint - datapoint id (e.g. idp0000)
  * @param {*} value - the value the datapoint should be set to
- * @param {boolean} update - should a status update be triggered
+ * @param {boolean} [update] - should a status update be triggered (optional)
  */
 var setDatapoint = function (serialNumberUnique, channel, datapoint, value, update) {
 	if (!data.actuators[serialNumberUnique]) {
@@ -216,7 +216,27 @@ var createActuator = function (serialNumberUnique, serialNumber, deviceId, typeN
 	return true;
 }
 
+/**
+ * reads a complete actuator structure and returns it
+ * 
+ * @param {string} serialNumberUnique - the unique identifier for the actuator; can be different from serialNumber if serialNumber is not unique (e.g. with Busch Jäger SysAP Scenes)
+ * @param {string} [channel] - channel id (e.g. ch0000)
+ * @param {string} [datapoint] - datapoint id (e.g. idp0000)
+ * 
+ * @returns {Object} - the complete data structure of the request
+ */
+var getActuatorData = function (serialNumberUnique, channel, datapoint) {
+	if (channel && datapoint) {
+		return data.actuators[serialNumberUnique]['channels'][channel]['datapoints'][datapoint];
+	} else if (channel) {
+		return data.actuators[serialNumberUnique]['channels'][channel]['datapoints'];
+	} else {
+		return data.actuators[serialNumberUnique]['channels'];
+	}
+}
+
 module.exports.setDatapoint = setDatapoint;
 module.exports.getData = getData;
 module.exports.setData = setData;
 module.exports.createActuator = createActuator;
+module.exports.getActuatorData = getActuatorData;

weather.js
@@ -19,7 +19,6 @@ var updateWeather = function () {
 			raw += d;
 		});
 		response.on('end', function () {
-			helper.log.info('weather updated');
 			var json = JSON.parse(raw);
 			helper.log.trace(json);
 			var temp = { max: -999, min: 999 };
@@ -28,7 +27,10 @@ var updateWeather = function () {
 				temp.min = Math.min(temp.min, item.temperature);
 			});
 			json.hourly.temperature = temp;
-			data.setData('weather', json);
+			Object.keys(json).forEach(function (key) {
+				data.setDatapoint('weather', 'ch0000', key, json[key]);
+			});
+			helper.log.info('weather updated');
 		});
 	}).on('error', function (e) {
 		helper.log.error('error updating weather: ' + e.message);

socketapi.js
@@ -77,7 +77,7 @@ function set (d, conn) {
 			break;
 		
 		case 'weather':
-			var weather = data.getData('weather');
+			var weather = data.getActuatorData('weather', 'ch0000');
 			conn.sendText(JSON.stringify({'weather': weather}));
 			break;
 		

commit 7a1a588d4f7996f3ba15e3c6a0b4ebdc0907291c

fix: weather is now again automatically pushed to the clients; bug introduced in 3488c1324ad74b1e42e2635d5dea14eb7674d344

data.js
@@ -18,6 +18,8 @@ format of the data.json file and the coresponding data object
 actuators: 
 	all actuators and their status; this can come from multiple sources
 	general format: serialnumber -> channel -> datapoint -> value
+	There is a special case for weather. It is stored as an actuator with the sn set to 
+	weather and all data is in the datapoint of ch0000
 	actuators: {
 		SERIALNUMBER: {
 			serialNumber: "SERIALNUMBER",
@@ -30,6 +32,17 @@ actuators:
 					}
 				},
 				...
+			}
+		},
+		weather: {
+			serialNumber: "weather",
+			channels: {
+				ch0000: {
+					datapoints: {
+						contains a data response from the Dark Sky API
+						see here for the documentation: https://darksky.net/dev/docs
+					}
+				}
 			}
 		}
 	}
@@ -107,10 +120,6 @@ structure:
 		...
 		]
 	}
-
-weather:
-	contains a data response from the Dark Sky API
-	see here for the documentation: https://darksky.net/dev/docs
 
 daynight:
 	contains the sunset & sunrise data
@@ -194,6 +203,24 @@ var setData = function (what, whatData) {
 }
 
 /**
+ * sends out a weather update
+ * weather is stored as an actuator with the sn set to 'weather' and the data is in ch0000
+ * 
+ * @param {boolean} broadcast - if set to true it will broadcast the weather data to all connected websocket clients
+ * 
+ * @returns {Object} - the complete weather data
+ */
+var getWeather = function (broadcast) {
+	var weather = getActuatorData('weather', 'ch0000');
+	if (broadcast) {
+		var sendData = {};
+		sendData['weather'] = weather;
+		websocket.broadcast(JSON.stringify(sendData));
+	}
+	return weather;
+}
+
+/**
  * adds an actuator to the master data structure
  *  
  * @param {string} serialNumberUnique - the unique identifier for the actuator; can be different from serialNumber if serialNumber is not unique (e.g. with Busch Jäger SysAP Scenes)
@@ -240,3 +267,4 @@ module.exports.getData = getData;
 module.exports.setData = setData;
 module.exports.createActuator = createActuator;
 module.exports.getActuatorData = getActuatorData;
+module.exports.getWeather = getWeather;

weather.js
@@ -27,9 +27,12 @@ var updateWeather = function () {
 				temp.min = Math.min(temp.min, item.temperature);
 			});
 			json.hourly.temperature = temp;
+			// weather is stored in datapoints so it can be used in triggers
 			Object.keys(json).forEach(function (key) {
 				data.setDatapoint('weather', 'ch0000', key, json[key]);
 			});
+			// broadcasts the weather
+			data.getWeather(true);
 			helper.log.info('weather updated');
 		});
 	}).on('error', function (e) {