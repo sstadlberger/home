32e9f10f302a63e5160bab367bdf6ba7e1d6dd64
Frank Eijking <frank@medianique.nl>
2019-03-14 12:16:18 +0100

    Add configuration for api custom port numbers

--- config/config.default.js
+++ config/config.default.js
@@ -1,28 +1,41 @@
+var SYSAP_HOST = 'SYSAP_HOSTNAME_OR_IP';
+var SYSAP_JID = 'JID@busch-jaeger.de';
+var SYSAP_PASSWORD = 'PASSWORD';
+
+var SOCKET_API_PORT = 8001;
+var WEB_API_PORT = 8080;
+
 var config = {};
+
+config.socketapi = {
+	port: SOCKET_API_PORT
+};
+
+config.webapi = {
+	port: WEB_API_PORT
+};
 
 // config for Busch Jäger free@home
 config.bosh = {
-	url: 'http://URL_TO_SYSAP:5280/http-bind',
-	jid: 'JID@busch-jaeger.de',
+	url: 'http://' + SYSAP_HOST + ':5280/http-bind',
+	jid: SYSAP_URL,
 	resource: 'nodeapi',
-	password: 'PASSWORD'
+	password: SYSAP_PASSWORD
 };
 
 config.websocket = {
-	url: 'ws://URL_TO_SYSAP:5280/xmpp-websocket/',
-	jid: 'JID@busch-jaeger.de',
+	url: 'ws://' + SYSAP_HOST + ':5280/xmpp-websocket/',
+	jid: SYSAP_JID,
 	resource: 'nodeapi',
-	password: 'PASSWORD'
+	password: SYSAP_PASSWORD
 };
-
 
 // config for Homematic
 config.homematic = {
 	ccuIP: 'CCUIP',
 	ccuPort: '2001',
 	localPort: '8030'
-}
-
+};
 
 // used for weather (optional)
 config.location = {
